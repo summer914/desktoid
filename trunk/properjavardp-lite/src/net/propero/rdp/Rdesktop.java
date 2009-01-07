1c38720448415693fbefc2887142f713653f6b99
pv@swooby.com <pv@swooby.com@9efe0888-dc52-11dd-993c-4d20d8c3c59d>
2009-01-07 16:27:05 +0000

    Removing features not usable in Android: showTools, toolFrame, usage, getopt, RDPClientChooser, Applet/JRdpLoader

--- trunk/properjavardp-lite/src/net/propero/rdp/Rdesktop.java
+++ trunk/properjavardp-lite/src/net/propero/rdp/Rdesktop.java
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 Propero Limited
  *
  * Purpose: Main class, launches session
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at
@@ -27,11 +27,7 @@
  * (See gpl.txt for details of the GNU General Public License.)
  * 
  */
-
 package net.propero.rdp;
-
-import gnu.getopt.Getopt;
-import gnu.getopt.LongOpt;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -44,12 +40,10 @@ import net.propero.rdp.keymapping.KeyCode_FileBased;
 import net.propero.rdp.rdp5.Rdp5;
 import net.propero.rdp.rdp5.VChannels;
 import net.propero.rdp.rdp5.cliprdr.ClipChannel;
-import net.propero.rdp.tools.SendEvent;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 public class Rdesktop {
 
@@ -204,65 +198,11 @@ public class Rdesktop {
 
 	static boolean readytosend;
 
-	static boolean showTools;
-
 	static final String keyMapPath = "keymaps/";
 
 	static String mapFile = "en-gb";
 
 	static String keyMapLocation = "";
-
-	static SendEvent toolFrame = null;
-
-	/**
-	 * Outputs version and usage information via System.err
-	 * 
-	 */
-	public static void usage() {
-		System.err.println("properJavaRDP version " + Version.version);
-		System.err
-				.println("Usage: java net.propero.rdp.Rdesktop [options] server[:port]");
-		System.err
-				.println("	-b 							bandwidth saving (good for 56k modem, but higher latency");
-		System.err.println("	-c DIR						working directory");
-		System.err.println("	-d DOMAIN					logon domain");
-		System.err
-				.println("	-f[l]						full-screen mode [with Linux KDE optimization]");
-		System.err.println("	-g WxH						desktop geometry");
-		System.err
-				.println("	-m MAPFILE					keyboard mapping file for terminal server");
-		System.err
-				.println("	-l LEVEL					logging level {DEBUG, INFO, WARN, ERROR, FATAL}");
-		System.err.println("	-n HOSTNAME					client hostname");
-		System.err.println("	-p PASSWORD					password");
-		System.err.println("	-s SHELL					shell");
-		System.err.println("	-t NUM						RDP port (default 3389)");
-		System.err.println("	-T TITLE					window title");
-		System.err.println("	-u USERNAME					user name");
-		System.err.println("	-o BPP						bits-per-pixel for display");
-		System.err
-				.println("    -r path                     path to load licence from (requests and saves licence from server if not found)");
-		System.err
-				.println("    --save_licence              request and save licence from server");
-		System.err
-				.println("    --load_licence              load licence from file");
-		System.err
-				.println("    --console                   connect to console");
-		System.err
-				.println("	--debug_key 				show scancodes sent for each keypress etc");
-		System.err.println("	--debug_hex 				show bytes sent and received");
-		System.err.println("	--no_remap_hash 			disable hash remapping");
-		System.err.println("	--quiet_alt 				enable quiet alt fix");
-		System.err
-				.println("	--no_encryption				disable encryption from client to server");
-		System.err.println("	--use_rdp4					use RDP version 4");
-		// System.err.println(" --enable_menu enable menu bar");
-		System.err
-				.println("	--log4j_config=FILE			use FILE for log4j configuration");
-		System.err
-				.println("Example: java net.propero.rdp.Rdesktop -g 800x600 -l WARN m52.propero.int");
-		Rdesktop.exit(0, null, null, true);
-	}
 
 	/**
 	 * 
@@ -270,276 +210,40 @@ public class Rdesktop {
 	 * @throws OrderException
 	 * @throws RdesktopException
 	 */
-	public static void main(String[] args) throws RdesktopException {
+	public static void main(String[] args) throws OrderException,
+			RdesktopException {
+
+		BasicConfigurator.configure();
 
 		// Ensure that static variables are properly initialised
 		keep_running = true;
 		loggedon = false;
 		readytosend = false;
-		showTools = false;
 		mapFile = "en-gb";
 		keyMapLocation = "";
-		toolFrame = null;
 
-		BasicConfigurator.configure();
-		logger.setLevel(Level.INFO);
+		logger.setLevel(Level.DEBUG);
 
-		// Attempt to run a native RDP Client
-
-		RDPClientChooser Chooser = new RDPClientChooser();
-
-		if (Chooser.RunNativeRDPClient(args)) {
-			if (!Common.underApplet)
-				System.exit(0);
-		}
-
-		// Failed to run native client, drop back to Java client instead.
-
-		// parse arguments
-
+		String server = "swwwooby";
 		int logonflags = Rdp.RDP_LOGON_NORMAL;
+		boolean fFullscreen = false;
+		boolean fFullscreenKdeHack = false;
 
-		boolean fKdeHack = false;
-		int c;
-		String arg;
-		StringBuffer sb = new StringBuffer();
-		LongOpt[] alo = new LongOpt[15];
-		alo[0] = new LongOpt("debug_key", LongOpt.NO_ARGUMENT, null, 0);
-		alo[1] = new LongOpt("debug_hex", LongOpt.NO_ARGUMENT, null, 0);
-		alo[2] = new LongOpt("no_paste_hack", LongOpt.NO_ARGUMENT, null, 0);
-		alo[3] = new LongOpt("log4j_config", LongOpt.REQUIRED_ARGUMENT, sb, 0);
-		alo[4] = new LongOpt("packet_tools", LongOpt.NO_ARGUMENT, null, 0);
-		alo[5] = new LongOpt("quiet_alt", LongOpt.NO_ARGUMENT, sb, 0);
-		alo[6] = new LongOpt("no_remap_hash", LongOpt.NO_ARGUMENT, null, 0);
-		alo[7] = new LongOpt("no_encryption", LongOpt.NO_ARGUMENT, null, 0);
-		alo[8] = new LongOpt("use_rdp4", LongOpt.NO_ARGUMENT, null, 0);
-		alo[9] = new LongOpt("use_ssl", LongOpt.NO_ARGUMENT, null, 0);
-		alo[10] = new LongOpt("enable_menu", LongOpt.NO_ARGUMENT, null, 0);
-		alo[11] = new LongOpt("console", LongOpt.NO_ARGUMENT, null, 0);
-		alo[12] = new LongOpt("load_licence", LongOpt.NO_ARGUMENT, null, 0);
-		alo[13] = new LongOpt("save_licence", LongOpt.NO_ARGUMENT, null, 0);
-		alo[14] = new LongOpt("persistent_caching", LongOpt.NO_ARGUMENT, null,
-				0);
-
-		String progname = "properJavaRDP";
-
-		Getopt g = new Getopt("properJavaRDP", args,
-				"bc:d:f::g:k:l:m:n:p:s:t:T:u:o:r:", alo);
-
-		ClipChannel clipChannel = new ClipChannel();
-
-		while ((c = g.getopt()) != -1) {
-			switch (c) {
-
-			case 0:
-				switch (g.getLongind()) {
-				case 0:
-					Options.debug_keyboard = true;
-					break;
-				case 1:
-					Options.debug_hexdump = true;
-					break;
-				case 2:
-					break;
-				case 3:
-					arg = g.getOptarg();
-					PropertyConfigurator.configure(arg);
-					logger.info("Log4j using config file " + arg);
-					break;
-				case 4:
-					showTools = true;
-					break;
-				case 5:
-					Options.altkey_quiet = true;
-					break;
-				case 6:
-					Options.remap_hash = false;
-					break;
-				case 7:
-					Options.packet_encryption = false;
-					break;
-				case 8:
-					Options.use_rdp5 = false;
-					// Options.server_bpp = 8;
-					Options.set_bpp(8);
-					break;
-				case 9:
-					Options.use_ssl = true;
-					break;
-				case 10:
-					Options.enable_menu = true;
-					break;
-				case 11:
-					Options.console_session = true;
-					break;
-				case 12:
-					Options.load_licence = true;
-					break;
-				case 13:
-					Options.save_licence = true;
-					break;
-				case 14:
-					Options.persistent_bitmap_caching = true;
-					break;
-				default:
-					usage();
-				}
-				break;
-
-			case 'o':
-				Options.set_bpp(Integer.parseInt(g.getOptarg()));
-				break;
-			case 'b':
-				Options.low_latency = false;
-				break;
-			case 'm':
-				mapFile = g.getOptarg();
-				break;
-			case 'c':
-				Options.directory = g.getOptarg();
-				break;
-			case 'd':
-				Options.domain = g.getOptarg();
-				break;
-			case 'f':
-				Dimension screen_size = Toolkit.getDefaultToolkit()
-						.getScreenSize();
-				// ensure width a multiple of 4
-				Options.width = screen_size.width & ~3;
-				Options.height = screen_size.height;
-				Options.fullscreen = true;
-				arg = g.getOptarg();
-				if (arg != null) {
-					if (arg.charAt(0) == 'l')
-						fKdeHack = true;
-					else {
-						System.err.println(progname
-								+ ": Invalid fullscreen option '" + arg + "'");
-						usage();
-					}
-				}
-				break;
-			case 'g':
-				arg = g.getOptarg();
-				int cut = arg.indexOf("x", 0);
-				if (cut == -1) {
-					System.err.println(progname + ": Invalid geometry: " + arg);
-					usage();
-				}
-				Options.width = Integer.parseInt(arg.substring(0, cut)) & ~3;
-				Options.height = Integer.parseInt(arg.substring(cut + 1));
-				break;
-			case 'k':
-				arg = g.getOptarg();
-				// Options.keylayout = KeyLayout.strToCode(arg);
-				if (Options.keylayout == -1) {
-					System.err.println(progname + ": Invalid key layout: "
-							+ arg);
-					usage();
-				}
-				break;
-			case 'l':
-				arg = g.getOptarg();
-				switch (arg.charAt(0)) {
-				case 'd':
-				case 'D':
-					logger.setLevel(Level.DEBUG);
-					break;
-				case 'i':
-				case 'I':
-					logger.setLevel(Level.INFO);
-					break;
-				case 'w':
-				case 'W':
-					logger.setLevel(Level.WARN);
-					break;
-				case 'e':
-				case 'E':
-					logger.setLevel(Level.ERROR);
-					break;
-				case 'f':
-				case 'F':
-					logger.setLevel(Level.FATAL);
-					break;
-				default:
-					System.err.println(progname + ": Invalid debug level: "
-							+ arg.charAt(0));
-					usage();
-				}
-				break;
-			case 'n':
-				Options.hostname = g.getOptarg();
-				break;
-			case 'p':
-				Options.password = g.getOptarg();
-				logonflags |= Rdp.RDP_LOGON_AUTO;
-				break;
-			case 's':
-				Options.command = g.getOptarg();
-				break;
-			case 'u':
-				Options.username = g.getOptarg();
-				break;
-			case 't':
-				arg = g.getOptarg();
-				try {
-					Options.port = Integer.parseInt(arg);
-				} catch (NumberFormatException nex) {
-					System.err.println(progname + ": Invalid port number: "
-							+ arg);
-					usage();
-				}
-				break;
-			case 'T':
-				Options.windowTitle = g.getOptarg().replace('_', ' ');
-				break;
-			case 'r':
-				Options.licence_path = g.getOptarg();
-				break;
-
-			case '?':
-			default:
-				usage();
-				break;
-
-			}
-		}
-
-		if (fKdeHack) {
-			Options.height -= 46;
-		}
-
-		String server = null;
-
-		if (g.getOptind() < args.length) {
-			int colonat = args[args.length - 1].indexOf(":", 0);
-			if (colonat == -1) {
-				server = args[args.length - 1];
-			} else {
-				server = args[args.length - 1].substring(0, colonat);
-				Options.port = Integer.parseInt(args[args.length - 1]
-						.substring(colonat + 1));
+		if (fFullscreen) {
+			Dimension screen_size = Toolkit.getDefaultToolkit().getScreenSize();
+			// ensure width a multiple of 4
+			Options.width = screen_size.width & ~3;
+			Options.height = screen_size.height;
+			Options.fullscreen = true;
+			if (fFullscreenKdeHack) {
+				Options.height -= 46;
 			}
 		} else {
-			System.err.println(progname + ": A server name is required!");
-			usage();
+			Options.width = 800;
+			Options.height = 600;
 		}
-
-		VChannels channels = new VChannels();
-
-		// Initialise all RDP5 channels
-		if (Options.use_rdp5) {
-			// TODO: implement all relevant channels
-			if (Options.map_clipboard)
-				channels.register(clipChannel);
-		}
-
-		// Now do the startup...
 
 		logger.info("properJavaRDP version " + Version.version);
-
-		if (args.length == 0)
-			usage();
 
 		String java = System.getProperty("java.specification.version");
 		logger.info("Java version is " + java);
@@ -547,20 +251,28 @@ public class Rdesktop {
 		String os = System.getProperty("os.name");
 		String osvers = System.getProperty("os.version");
 
-		if (os.equals("Windows 2000") || os.equals("Windows XP"))
-			Options.built_in_licence = true;
-
 		logger.info("Operating System is " + os + " version " + osvers);
 
 		if (os.startsWith("Linux"))
 			Constants.OS = Constants.LINUX;
-		else if (os.startsWith("Windows"))
+		else if (os.startsWith("Windows")) {
+			Options.built_in_licence = true;
 			Constants.OS = Constants.WINDOWS;
-		else if (os.startsWith("Mac"))
+		} else if (os.startsWith("Mac"))
 			Constants.OS = Constants.MAC;
 
-		if (Constants.OS == Constants.MAC)
-			Options.caps_sends_up_and_down = false;
+		// Now do the startup...
+
+		VChannels channels = new VChannels();
+		ClipChannel clipChannel = new ClipChannel();
+
+		// Initialise all RDP5 channels
+		if (Options.use_rdp5) {
+			// TODO: implement all relevant channels
+			if (Options.map_clipboard) {
+				channels.register(clipChannel);
+			}
+		}
 
 		Rdp5 RdpLayer = null;
 		Common.rdp = RdpLayer;
@@ -590,7 +302,6 @@ public class Rdesktop {
 			kmEx.printStackTrace();
 			Rdesktop.exit(0, null, null, true);
 		}
-
 		logger.debug("Registering keyboard...");
 		if (keyMap != null)
 			window.registerKeyboard(keyMap);
@@ -623,21 +334,13 @@ public class Rdesktop {
 							.getByName(server), logonflags, Options.domain,
 							Options.password, Options.command,
 							Options.directory);
-
-					// Remove to get rid of sendEvent tool
-					if (showTools) {
-						toolFrame = new SendEvent(RdpLayer);
-						toolFrame.show();
-					}
-					// End
-
 					if (keep_running) {
 
-						/*
-						 * By setting encryption to False here, we have an
-						 * encrypted login packet but unencrypted transfer of
-						 * other packets
-						 */
+						//
+						// By setting encryption to False here, we have an
+						// encrypted login packet but unencrypted transfer of
+						// other packets
+						//
 						if (!Options.packet_encryption)
 							Options.encryption = false;
 
@@ -646,20 +349,15 @@ public class Rdesktop {
 						RdpLayer.mainLoop(deactivated, ext_disc_reason);
 
 						if (deactivated[0]) {
-							/* clean disconnect */
+							// clean disconnect
 							Rdesktop.exit(0, RdpLayer, window, true);
-							// return 0;
 						} else {
 							if (ext_disc_reason[0] == exDiscReasonAPIInitiatedDisconnect
 									|| ext_disc_reason[0] == exDiscReasonAPIInitiatedLogoff) {
-								/*
-								 * not so clean disconnect, but nothing to worry
-								 * about
-								 */
+								// not so clean disconnect, but nothing to worry
+								// about
 								Rdesktop.exit(0, RdpLayer, window, true);
-								// return 0;
 							}
-
 							if (ext_disc_reason[0] >= 2) {
 								String reason = textDisconnectReason(ext_disc_reason[0]);
 								String msg[] = { "Connection terminated",
@@ -668,13 +366,11 @@ public class Rdesktop {
 								logger.warn("Connection terminated: " + reason);
 								Rdesktop.exit(0, RdpLayer, window, true);
 							}
-
 						}
-
 						keep_running = false; // exited main loop
 						if (!readytosend) {
 							// maybe the licence server was having a comms
-							// problem, retry?
+							// problem; retry?
 							String msg1 = "The terminal server disconnected before licence negotiation completed.";
 							String msg2 = "Possible cause: terminal server could not issue a licence.";
 							String[] msg = { msg1, msg2 };
@@ -682,12 +378,7 @@ public class Rdesktop {
 							logger.warn(msg2);
 							window.showErrorDialog(msg);
 						}
-					} // closing bracket to if(running)
-
-					// Remove to get rid of tool window
-					if (showTools)
-						toolFrame.dispose();
-					// End
+					}
 
 				} catch (ConnectionException e) {
 					String msg[] = { "Connection Exception", e.getMessage() };
@@ -711,8 +402,8 @@ public class Rdesktop {
 					e.printStackTrace(System.err);
 
 					if (!readytosend) {
-						// maybe the licence server was having a comms
-						// problem, retry?
+						// maybe the licence server was having a comms problem,
+						// retry?
 						String msg[] = {
 								"The terminal server reset connection before licence negotiation completed.",
 								"Possible cause: terminal server could not connect to licence server.",
@@ -741,7 +432,7 @@ public class Rdesktop {
 					e.printStackTrace();
 					error(e, RdpLayer, window, true);
 				}
-			} else { // closing bracket to if(!rdp==null)
+			} else {
 				logger
 						.fatal("The communications layer could not be initiated!");
 			}
@@ -764,11 +455,6 @@ public class Rdesktop {
 	public static void exit(int n, Rdp rdp, RdesktopFrame window,
 			boolean sysexit) {
 		keep_running = false;
-
-		// Remove to get rid of tool window
-		if ((showTools) && (toolFrame != null))
-			toolFrame.dispose();
-		// End
 
 		if (rdp != null && rdp.isConnected()) {
 			logger.info("Disconnecting ...");
